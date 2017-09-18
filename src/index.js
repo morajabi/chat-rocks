ab8792cdd67fd7b552214fcd3a28af5faa8519fe
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-09-19 00:09:06 +0430

    feat: add subscribtions

--- src/index.js
+++ src/index.js
@@ -5,15 +5,33 @@ import {
   ApolloProvider,
   createNetworkInterface,
 } from 'react-apollo'
+import { SubscriptionClient } from 'subscriptions-transport-ws'
+import { addGraphQLSubscriptions } from 'add-graphql-subscriptions'
+// We should upgrade to apollo v2 and apollo-link and latest
+// subscriptions-transport-ws instead of these three packages
 
 import App from './components/App'
+
+// Setup socket client for subscriptions
+const wsClient = new SubscriptionClient(
+  `wss://subscriptions.graph.cool/v1/chat-rocks`,
+  {
+    reconnect: true
+  }
+)
 
 const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/chat-rocks',
 })
 
-const apolloClient = new ApolloClient({
+// Extend the network interface with the WebSocket
+const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
   networkInterface,
+  wsClient
+)
+
+const apolloClient = new ApolloClient({
+  networkInterface: networkInterfaceWithSubscriptions,
 })
 
 ReactDOM.render(
