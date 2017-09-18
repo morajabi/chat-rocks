a19649bc022784d4ffdf2ac5cf250d2a576f2429
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-09-18 19:59:18 +0430

    feat: setup apollo

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Message from './Message'
 
 const Chat = ({ messages = [] }) => (
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {
+  ApolloClient,
+  ApolloProvider,
+  createNetworkInterface,
+} from 'react-apollo'
+
 import App from './components/App'
 
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/chat-rocks',
+})
+
+const apolloClient = new ApolloClient({
+  networkInterface,
+})
+
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={apolloClient}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 )
