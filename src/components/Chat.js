66ca916a24375aa6f0896d978fc5f47204c47eb6
Mohammad Rajabifard <mo.rajbi@gmail.com>
2017-09-19 01:04:09 +0430

    connect Chat to subscriptions api, Now it's Realtime!

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { gql, graphql } from 'react-apollo'
 
 import Message from './Message'
 
-const Chat = ({ data: { allMessages = [], loading = false } }) => {
-  if (loading) {
-    return <div>loading...</div>
+class Chat extends PureComponent {
+  componentDidMount() {
+    this.props.subscribeToNewMessage()
   }
 
-  return (
-    <div>
-      {allMessages.map((msg, i) => (
-        <Message key={i}>{msg.content}</Message>
-      ))}
-    </div>
-  )
+  render() {
+    const { messages: { allMessages = [], loading = false } } = this.props
+
+    if (loading) {
+      return <div>loading...</div>
+    }
+
+    return (
+      <div>
+        {allMessages.map((msg, i) => (
+          <Message key={i}>{msg.content}</Message>
+        ))}
+      </div>
+    )
+  }
 }
 
 const getAllMessages = gql`query allMessages {
@@ -23,4 +31,46 @@ const getAllMessages = gql`query allMessages {
   }
 }`
 
-export default graphql(getAllMessages)(Chat)
+
+const messageSubscription = gql`subscription messageSubscription {
+  Message {
+    node {
+      content
+    }
+  }
+}`
+
+const withData = graphql(getAllMessages, {
+  name: 'messages',
+  props: props => {
+    return {
+      messages: props.messages,
+      subscribeToNewMessage: params => {
+        return props.messages.subscribeToMore({
+          document: messageSubscription,
+          variables: null,
+
+          updateQuery: (prev, { subscriptionData }) => {
+            if (!subscriptionData.data) {
+              return prev;
+            }
+
+            const newMessage = subscriptionData.data.Message.node;
+
+            return {
+              allMessages: [
+                {
+                  ...newMessage
+                },
+                ...prev.allMessages
+              ]
+            }
+          }
+        })
+      }
+    }
+  }
+})
+
+// export default graphql(getAllMessages)(Chat)
+export default withData(Chat)
